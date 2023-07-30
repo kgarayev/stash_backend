fc1f0da145e0694bbaae21de1d0ac93579e273d3
kgarayev <kenangarayev@gmail.com>
2023-07-30 14:06:43 +0100

    some unnecessary things commented out

--- routes/account.ts
+++ routes/account.ts
@@ -46,220 +46,220 @@ interface DatabaseEntry {
 
 // GET ROUTE:
 // get a specific account router
-router.get("/:id", async (req, res) => {
-  // convert id from string to number
-  const id = Number(req.params.id);
+// router.get("/:id", async (req, res) => {
+//   // convert id from string to number
+//   const id = Number(req.params.id);
 
-  // check if the id is number
-  if (Number.isNaN(id)) {
-    res.send({ status: 0, reason: "Invalid id" });
-    return;
-  }
+//   // check if the id is number
+//   if (Number.isNaN(id)) {
+//     res.send({ status: 0, reason: "Invalid id" });
+//     return;
+//   }
 
-  // ask sql for data
-  // returns an array of results
-  const results = (await asyncMySQL(
-    getQuery("accounts", id)
-  )) as DatabaseEntry[];
+//   // ask sql for data
+//   // returns an array of results
+//   const results = (await asyncMySQL(
+//     getQuery("accounts", id)
+//   )) as DatabaseEntry[];
 
-  // check if the results are there
-  if (results.length > 0) {
-    res.send({ status: 1, results });
-    return;
-  }
+//   // check if the results are there
+//   if (results.length > 0) {
+//     res.send({ status: 1, results });
+//     return;
+//   }
 
-  // if the resuts are not there, communicate this
-  res.send({ status: 0, reason: "Id not found" });
-});
+//   // if the resuts are not there, communicate this
+//   res.send({ status: 0, reason: "Id not found" });
+// });
 
 // POST ROUTE:
 // add account router
-router.post("/", async (req, res) => {
-  // just console log the body
-  console.log(req.body);
+// router.post("/", async (req, res) => {
+//   // just console log the body
+//   console.log(req.body);
 
-  // validate
-  let localErrors = await validate(req.body, "addAccount");
+//   // validate
+//   let localErrors = await validate(req.body, "addAccount");
 
-  // log local errors if any
-  console.log(localErrors);
+//   // log local errors if any
+//   console.log(localErrors);
 
-  // notify about validation errors and abort if any
-  if (localErrors) {
-    res.send({ status: 0, reason: "Incomplete or invalid request" });
-    return;
-  }
+//   // notify about validation errors and abort if any
+//   if (localErrors) {
+//     res.send({ status: 0, reason: "Incomplete or invalid request" });
+//     return;
+//   }
 
-  //   destructuring the body
-  const {
-    accountName,
-    accountNumber,
-    sortCode,
-    currencyCode,
-    currencyName,
-    currencySymbol,
-    currencyCountry,
-    balance,
-    userId,
-  } = req.body;
+//   //   destructuring the body
+//   const {
+//     accountName,
+//     accountNumber,
+//     sortCode,
+//     currencyCode,
+//     currencyName,
+//     currencySymbol,
+//     currencyCountry,
+//     balance,
+//     userId,
+//   } = req.body;
 
-  // implementing the query
-  try {
-    await asyncMySQL(
-      addAccount(
-        accountName,
-        accountNumber,
-        sortCode,
-        currencyCode,
-        currencyName,
-        currencySymbol,
-        currencyCountry,
-        balance,
-        userId
-      )
-    );
-    // notifying the front of successful result
-    res.send({ status: 1, message: "Account added" });
-    return;
-  } catch (error) {
-    // error message to the front
-    res.send({ status: 0, reason: (error as any)?.sqlMessage });
-    return;
-  }
-});
+//   // implementing the query
+//   try {
+//     await asyncMySQL(
+//       addAccount(
+//         accountName,
+//         accountNumber,
+//         sortCode,
+//         currencyCode,
+//         currencyName,
+//         currencySymbol,
+//         currencyCountry,
+//         balance,
+//         userId
+//       )
+//     );
+//     // notifying the front of successful result
+//     res.send({ status: 1, message: "Account added" });
+//     return;
+//   } catch (error) {
+//     // error message to the front
+//     res.send({ status: 0, reason: (error as any)?.sqlMessage });
+//     return;
+//   }
+// });
 
 // DELETE ROUTE:
 // delete an account router
-router.delete("/:id", async (req, res) => {
-  // converting id from string to number
-  const id = Number(req.params.id);
+// router.delete("/:id", async (req, res) => {
+//   // converting id from string to number
+//   const id = Number(req.params.id);
 
-  // check if the id is number
-  if (Number.isNaN(id)) {
-    res.send({ status: 0, reason: "Invalid id" });
-    return;
-  }
+//   // check if the id is number
+//   if (Number.isNaN(id)) {
+//     res.send({ status: 0, reason: "Invalid id" });
+//     return;
+//   }
 
-  try {
-    // run the query
-    const result = (await asyncMySQL(deleteQuery("accounts", id))) as any;
+//   try {
+//     // run the query
+//     const result = (await asyncMySQL(deleteQuery("accounts", id))) as any;
 
-    console.log(result);
+//     console.log(result);
 
-    // check if the id exists and the account has been removed
-    if (result.affectedRows === 1) {
-      // send the successful update to the front
-      res.send({ status: 1, message: "Account removed" });
-      return;
-    }
-    // if not, notify the front
-    res.send({ status: 0, message: "Invalid id" });
-    return;
-  } catch (error) {
-    // catch the error
-    res.send({ status: 0, reason: (error as any)?.sqlMessage });
-    return;
-  }
-});
+//     // check if the id exists and the account has been removed
+//     if (result.affectedRows === 1) {
+//       // send the successful update to the front
+//       res.send({ status: 1, message: "Account removed" });
+//       return;
+//     }
+//     // if not, notify the front
+//     res.send({ status: 0, message: "Invalid id" });
+//     return;
+//   } catch (error) {
+//     // catch the error
+//     res.send({ status: 0, reason: (error as any)?.sqlMessage });
+//     return;
+//   }
+// });
 
 // UPDATE ROUTE:
 // router to update the account information
-router.patch("/:id", async (req, res) => {
-  // convert id from string to number
-  const id = Number(req.params.id);
+// router.patch("/:id", async (req, res) => {
+//   // convert id from string to number
+//   const id = Number(req.params.id);
 
-  // validate
-  let localErrors = await validate(req.body, "updateAccount");
+//   // validate
+//   let localErrors = await validate(req.body, "updateAccount");
 
-  // checking if local errors exist
-  if (localErrors) {
-    res.send({ status: 0, reason: "Incomplete or invalid request" });
-    return;
-  }
+//   // checking if local errors exist
+//   if (localErrors) {
+//     res.send({ status: 0, reason: "Incomplete or invalid request" });
+//     return;
+//   }
 
-  //   destructuring the body
-  const {
-    accountName,
-    accountNumber,
-    sortCode,
-    currencyCode,
-    currencyName,
-    currencySymbol,
-    currencyCountry,
-    balance,
-    userId,
-  } = req.body;
+//   //   destructuring the body
+//   const {
+//     accountName,
+//     accountNumber,
+//     sortCode,
+//     currencyCode,
+//     currencyName,
+//     currencySymbol,
+//     currencyCountry,
+//     balance,
+//     userId,
+//   } = req.body;
 
-  try {
-    // First, check if account with this id exists
-    const results = (await asyncMySQL(
-      `SELECT * FROM accounts WHERE id LIKE "${id}"`
-    )) as DatabaseEntry[];
+//   try {
+//     // First, check if account with this id exists
+//     const results = (await asyncMySQL(
+//       `SELECT * FROM accounts WHERE id LIKE "${id}"`
+//     )) as DatabaseEntry[];
 
-    // If no account exists with this id, return an error
-    if (results.length === 0) {
-      res.send({ status: 0, message: "Invalid account id" });
-      return;
-    }
+//     // If no account exists with this id, return an error
+//     if (results.length === 0) {
+//       res.send({ status: 0, message: "Invalid account id" });
+//       return;
+//     }
 
-    //   for security we have repetition
-    if (accountName && typeof accountName === "string") {
-      await asyncMySQL(
-        updateQuery("accounts", "account_name", accountName, id)
-      );
-    }
+//     //   for security we have repetition
+//     if (accountName && typeof accountName === "string") {
+//       await asyncMySQL(
+//         updateQuery("accounts", "account_name", accountName, id)
+//       );
+//     }
 
-    if (accountNumber && typeof Number(accountNumber) === "number") {
-      await asyncMySQL(
-        updateQuery("accounts", "account_number", accountNumber, id)
-      );
-    }
+//     if (accountNumber && typeof Number(accountNumber) === "number") {
+//       await asyncMySQL(
+//         updateQuery("accounts", "account_number", accountNumber, id)
+//       );
+//     }
 
-    if (sortCode && typeof Number(sortCode) === "number") {
-      await asyncMySQL(updateQuery("accounts", "sort_code", sortCode, id));
-    }
+//     if (sortCode && typeof Number(sortCode) === "number") {
+//       await asyncMySQL(updateQuery("accounts", "sort_code", sortCode, id));
+//     }
 
-    if (currencyCode && typeof currencyCode === "string") {
-      await asyncMySQL(
-        updateQuery("accounts", "currency_code", currencyCode, id)
-      );
-    }
+//     if (currencyCode && typeof currencyCode === "string") {
+//       await asyncMySQL(
+//         updateQuery("accounts", "currency_code", currencyCode, id)
+//       );
+//     }
 
-    if (currencyName && typeof currencyName === "string") {
-      await asyncMySQL(
-        updateQuery("accounts", "currency_name", currencyName, id)
-      );
-    }
+//     if (currencyName && typeof currencyName === "string") {
+//       await asyncMySQL(
+//         updateQuery("accounts", "currency_name", currencyName, id)
+//       );
+//     }
 
-    if (currencySymbol && typeof currencySymbol === "string") {
-      await asyncMySQL(
-        updateQuery("accounts", "currency_symbol", currencySymbol, id)
-      );
-    }
+//     if (currencySymbol && typeof currencySymbol === "string") {
+//       await asyncMySQL(
+//         updateQuery("accounts", "currency_symbol", currencySymbol, id)
+//       );
+//     }
 
-    if (currencyCountry && typeof currencyCountry === "string") {
-      await asyncMySQL(
-        updateQuery("accounts", "currency_country", currencyCountry, id)
-      );
-    }
+//     if (currencyCountry && typeof currencyCountry === "string") {
+//       await asyncMySQL(
+//         updateQuery("accounts", "currency_country", currencyCountry, id)
+//       );
+//     }
 
-    if (balance && typeof Number(balance) === "number") {
-      await asyncMySQL(updateQuery("accounts", "balance", balance, id));
-    }
+//     if (balance && typeof Number(balance) === "number") {
+//       await asyncMySQL(updateQuery("accounts", "balance", balance, id));
+//     }
 
-    if (userId && typeof Number(userId) === "number") {
-      await asyncMySQL(updateQuery("accounts", "user_id", userId, id));
-    }
+//     if (userId && typeof Number(userId) === "number") {
+//       await asyncMySQL(updateQuery("accounts", "user_id", userId, id));
+//     }
 
-    // sending the final update to the front
-    res.send({ status: 1, message: "Account updated" });
-    return;
-  } catch (error) {
-    // catch errors if any
-    res.send({ status: 0, reason: (error as any)?.sqlMessage });
-    return;
-  }
-});
+//     // sending the final update to the front
+//     res.send({ status: 1, message: "Account updated" });
+//     return;
+//   } catch (error) {
+//     // catch errors if any
+//     res.send({ status: 0, reason: (error as any)?.sqlMessage });
+//     return;
+//   }
+// });
 
 // exporting the router
 export { router };
--- routes/generic.ts
+++ routes/generic.ts
@@ -32,50 +32,50 @@ interface DatabaseEntry {
 // import router
 const router = express.Router();
 
-// route
-router.get("/", async (req, res) => {
-  // log the headers of the request
-  console.log("New request to: /", req.headers);
+// // route
+// router.get("/", async (req, res) => {
+//   // log the headers of the request
+//   console.log("New request to: /", req.headers);
 
-  // destructuring the request queries
-  let { num } = req.query;
+//   // destructuring the request queries
+//   let { num } = req.query;
 
-  console.log(num);
+//   console.log(num);
 
-  try {
-    var userQuantity = (
-      (await asyncMySQL(`SELECT COUNT(*) FROM users`)) as any
-    )[0]["COUNT(*)"];
-    console.log(userQuantity);
-  } catch (error) {
-    res.send({ status: 0, error });
-    return;
-  }
+//   try {
+//     var userQuantity = (
+//       (await asyncMySQL(`SELECT COUNT(*) FROM users`)) as any
+//     )[0]["COUNT(*)"];
+//     console.log(userQuantity);
+//   } catch (error) {
+//     res.send({ status: 0, error });
+//     return;
+//   }
 
-  // if a specific quantity is asked for
-  if (Number(num) > 0 && Number(num) < userQuantity) {
-    const results = await asyncMySQL(`SELECT * FROM users LIMIT ${num}`);
-    res.send({ status: 1, results });
-    return;
-  } else if (Number(num) === 0) {
-    res.send({ status: 1, message: "No users requested" });
-    return;
-  } else if (Number(num) < 0) {
-    res.send({ status: 0, message: "Invalid query" });
-    return;
-  }
+//   // if a specific quantity is asked for
+//   if (Number(num) > 0 && Number(num) < userQuantity) {
+//     const results = await asyncMySQL(`SELECT * FROM users LIMIT ${num}`);
+//     res.send({ status: 1, results });
+//     return;
+//   } else if (Number(num) === 0) {
+//     res.send({ status: 1, message: "No users requested" });
+//     return;
+//   } else if (Number(num) < 0) {
+//     res.send({ status: 0, message: "Invalid query" });
+//     return;
+//   }
 
-  // otherwise, show all users
-  const results = await asyncMySQL(
-    `SELECT * FROM users 
-      LEFT JOIN accounts 
-        ON users.id=accounts.user_id 
-          LEFT JOIN transactions 
-            ON accounts.id=transactions.account_id`
-  );
+//   // otherwise, show all users
+//   const results = await asyncMySQL(
+//     `SELECT * FROM users
+//       LEFT JOIN accounts
+//         ON users.id=accounts.user_id
+//           LEFT JOIN transactions
+//             ON accounts.id=transactions.account_id`
+//   );
 
-  // send the response to the front
-  res.send({ status: 1, results });
-});
+//   // send the response to the front
+//   res.send({ status: 1, results });
+// });
 
 export { router };
