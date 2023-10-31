dafd77220a11fe9816e9ae5ec03b4fa3b717c729
kgarayev <kenangarayev@gmail.com>
2023-10-31 22:34:45 +0000

    first connection

--- database/connection.ts
+++ database/connection.ts
@@ -1,6 +1,5 @@
 // import mysql
-import mysql from "mysql";
-
+import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,16 +13,17 @@ const {
   createTokensTable,
 } = queries;
 
+const { Pool } = pg;
+
 // instance of a connection
 // this driver does not support promises
 // need to create our own wrapper / promisify this
 // connect to RDMS without specifying the database itself
-const pool = mysql.createPool({
-  connectionLimit: 10,
+const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   database: process.env.DB_NAME,
 });
 
